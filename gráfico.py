f3de34b235a61f6f294335a945b9caaf6a524b64
GabrielC248 <gabrielccoelho10@gmail.com>
2024-09-29 14:42:55 -0300

    Added sympy

--- gráfico.py
+++ gráfico.py
@@ -1,9 +1,12 @@
 import numpy as np
+import sympy
 import matplotlib.pyplot as plt
 
 # Definir a função
-def f(x):
-    return x**3 - x - 1
+func = "x^3-x-1"
+xpy = sympy.symbols('x')
+func_py = sympy.sympify(func)
+f = sympy.lambdify(xpy, func_py)
 
 # Gerar valores de x
 x = np.linspace(-2, 2, 400)
@@ -13,20 +16,17 @@ y = f(x)
 
 # Criar o gráfico
 plt.figure(figsize=(8,6))
-plt.plot(x, y, label=r'$f(x) = x^3 - x - 1$', color='b')
+plt.plot(x, y, label = func, color='b')
 plt.axhline(0, color='black', linewidth=0.5)
 plt.axvline(0, color='black', linewidth=0.5)
-plt.title('Gráfico da função $f(x) = x^3 - x - 1$')
+plt.title("Gráfico da função:" + func)
 plt.xlabel('x')
 plt.ylabel('f(x)')
 plt.grid(True)
 plt.legend()
 
 # Salvar o gráfico como uma imagem PNG
-plt.savefig('grafico_funcao_x3_menos_x_menos_1.png')
+plt.savefig('grafico_funcao.png')
 
 # Mostrar o gráfico
 plt.show()
-
-
-teste
